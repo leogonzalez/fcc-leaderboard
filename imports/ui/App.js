3e7f45941de5c06904102ffb7dba6fba27807716
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-07-01 19:22:40 -0300

    Added link to repo

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,10 @@ export default () => {
     <div>
       <div>
         <h1>Creating a Leaderboard for FreeCodeCamp</h1>
+        <h5>
+          Code available at <a href='https://github.com/leogonzalez/fcc-leaderboard'>
+           https://github.com/leogonzalez/fcc-leaderboard</a>
+        </h5>
         <List/>
       </div>
     </div>
